2f33e527241cb2e5620ef6cc16ff2c694d5c789c
Alexeykormiltsev <92741522+Alexeykormiltsev@users.noreply.github.com>
2022-12-06 14:03:14 +0500

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+
+    $('.top-slider__inner').slick({
+        dots: false,
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 5000
+    });
+
+    $('.tarla__slider-inner').slick({
+        slidesToShow: 3.6,
+        slidesToScroll: 2,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+      });
+
+});
